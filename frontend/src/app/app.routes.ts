e2a3808a3d2a198d95af770ce4d2fab19ec46ead
Diego Lalanda <98861314+DiegoLalanda@users.noreply.github.com>
2025-05-11 18:15:54 -0300

    Revert "Implementación de páginas Welcome y Dashboard"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Routes } from '@angular/router';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [
-    {
-        path: '',
-        component: WelcomeComponent
-    },
-    { path: 'dashboard', component: DashboardComponent },
-];
+export const routes: Routes = [];
