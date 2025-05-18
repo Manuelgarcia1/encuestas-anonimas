40163c204aa9a386428c748bd2e30ab6ef63610e
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2025-05-18 17:42:34 -0300

    cambios aceptados

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,23 +6,12 @@ import { ResultsComponent } from './components/form/results/results.component';
 import { ResponseComponent } from './components/response/response.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
   {
     path: '',
     component: WelcomeComponent,
   },
   { path: 'dashboard', component: DashboardComponent },
-
   { path: 'create', component: CreateComponent },
   { path: 'results', component: ResultsComponent },
-=======
-    {
-        path: '',
-        component: WelcomeComponent
-    },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'create', component: CreateComponent },
-    { path: 'results', component: ResultsComponent },
-    {path:'response',component:ResponseComponent}
->>>>>>> 0d043583759f595b1745eb90b58c927eef4166fa
+  { path: 'response', component: ResponseComponent },
 ];
