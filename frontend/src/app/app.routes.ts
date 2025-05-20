2f5d888b2e7467377dc217410b80a41395b76339
FerElola <fernanda.elola@tiendamia.com>
2025-05-20 19:11:58 -0300

    fix conflicto y remuevo token en ruta

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,23 +6,9 @@ import { ResultsComponent } from './components/form/results/results.component';
 import { ResponseComponent } from './components/response/response.component';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-  {
-    path: '',
-    component: WelcomeComponent,
-  },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'create', component: CreateComponent },
-  { path: 'results', component: ResultsComponent },
-  { path: 'response', component: ResponseComponent },
-=======
-    {
-        path: '',
-        component: WelcomeComponent
-    },
+    { path: '', component: WelcomeComponent },
     { path: 'dashboard', component: DashboardComponent },
     { path: 'create/:id', component: CreateComponent },
     { path: 'results', component: ResultsComponent },
-    {path:'response',component:ResponseComponent}
->>>>>>> a5e486e9a6f6f4df65709a4091b453677e3d4994
+    { path:'response', component:ResponseComponent }
 ];
