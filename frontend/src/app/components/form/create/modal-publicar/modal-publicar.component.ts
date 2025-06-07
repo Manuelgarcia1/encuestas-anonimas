34f0521ee6cec50393e3f2afd25eae042d01c30c
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2025-06-07 17:09:34 -0300

    - Cambio para usar nginx

--- frontend/src/app/components/form/create/modal-publicar/modal-publicar.component.ts
+++ frontend/src/app/components/form/create/modal-publicar/modal-publicar.component.ts
@@ -51,7 +51,7 @@ export class ModalPublicarComponent implements OnInit {
     this.encuestasService.getTokenParticipacion(tokenDashboard, this.encuestaId).pipe(
       switchMap((responseParticipacion: any) => {
         if (responseParticipacion && responseParticipacion.data && responseParticipacion.data.token_respuesta) {
-          this.surveyLink = `http://localhost:4200/response/${responseParticipacion.data.token_respuesta}`;
+          this.surveyLink = `http://localhost/response/${responseParticipacion.data.token_respuesta}`;
         } else {
           // Si no hay token_respuesta, lanzamos un error para que lo capture el bloque 'error'
           throw new Error('No se pudo obtener el token de participación.');
